175e988a039476ab277a2d76c660ff6a3dbe01ce
gabriela <gabriela1yujra@gmail.com>
2022-05-19 01:33:38 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../style/App.css';
+import {Tarjeta1} from '../components/Tarjeta1.jsx'
+import {Tarjeta2} from '../components/Tarjeta2.jsx'
+
+function App() {
+    return (
+        <div className='App'>
+             <Tarjeta1/>
+             <Tarjeta2/>
+        </div>
+    );
+}
+
+export default App;
